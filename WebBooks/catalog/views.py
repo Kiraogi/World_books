efce090b2ba06404fcf4a5d8ab55269a6d9bad73
Kiraogi <golokron3@yandex.ru>
2024-03-25 22:03:34 +0300

    Ver 0.06

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Главная страница сайта Мир книг!")
+    # Словарь для передачи данных в шаблон
+    text_head = "Это заголовок главной страницы"
+    text_body = "Это содержимое главной страницы сайта"
+    context = {'text_head': text_head, 'text_body': text_body}
+    # Передача словоря context с данными в шаблоне
+    return render(request, 'catalog/index.html', context)
