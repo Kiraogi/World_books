0f7d54267df33724571ffb9e6909561369923b98
Kiraogi <golokron3@yandex.ru>
2024-03-27 16:32:14 +0300

    Ver 0.07

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
 
 def index(request):
-    # Словарь для передачи данных в шаблон
-    text_head = "Это заголовок главной страницы"
-    text_body = "Это содержимое главной страницы сайта"
-    context = {'text_head': text_head, 'text_body': text_body}
-    # Передача словоря context с данными в шаблоне
+    text_head = "На нашем сайте вы можете получить книги в электронном виде"
+    # Данные о книгах и их количестве
+    books = Book.objects.all()
+    num_books = Book.objects.all().count()
+    # Данные об экземплярах книг в БД
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'На складе')
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    # Данные об авторах книг
+    author = Author.objects
+    num_authors = Author.objects.count()
+    # Словарь для передачи данных в шаблон index.html
+    context = {
+        'text_head': text_head,
+        'books': books,
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'author': author,
+        'num_authors': num_authors
+    }
+    # Передача словаря context с данными в шаблон
     return render(request, 'catalog/index.html', context)
