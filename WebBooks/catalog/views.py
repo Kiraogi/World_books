3900cc9b9b8700bb72d6d91c5b81d3fa076e42fe
Kiraogi <golokron3@yandex.ru>
2024-03-30 20:41:03 +0300

    Ver 0.09

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -25,10 +25,28 @@ class BookListView(ListView):
     paginate_by = 3
 
 
-# def about(request):
-#     text_head = 'Сведения о компании'
-#     name = 'Компания "Мир Книг"'
-#     rab1 =
+def about(request):
+    text_head = 'Сведения о компании'
+    name = 'Компания "Мир Книг"'
+    rab1 = 'Продажа книжек'
+    rab2 = 'Покупка книжек'
+    rab3 = 'Доставка книжек'
+    rad4 = 'Оплата книжек'
+    context = {'text_head': text_head, 'name': name, 'rab1': rab1, 'rab2': rab2, 'rab3': rab3, 'rad4': rad4}
+    # передача словаря context с данными в шаблон
+    return render(request, 'catalog/about.html', context)
+
+
+def contact(request):
+    text_head = 'Контакты'
+    name = 'Компания "Мир Книг"'
+    address = 'Москва, ул. Ленина, д. 1'
+    tel = '8-800-555-35-35'
+    email = 'MirKnig@example.com'
+    # Словарь для передачи данных в шаблон index.html
+    context = {'text_head': text_head, 'name': name, 'address': address, 'tel': tel, 'email': email}
+    # передача словаря context с данными в шаблон
+    return render(request, 'catalog/contact.html', context)
 
 
 def index(request):
